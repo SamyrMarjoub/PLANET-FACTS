292dd0f82b5773f869a33541d8325acacd721914
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-05-28 20:05:32 -0300

    Add files via upload

---
+++ app/controllers/index.js
@@ -0,0 +1,24 @@
+module.exports.index = function (application, req, res) {
+    res.render('index')
+}
+module.exports.mars = function (application, req, res) {
+    res.render('mars')
+}
+module.exports.mercury = function (application, req, res) {
+    res.render('mercury')
+}
+module.exports.venus = function (application, req, res) {
+    res.render('venus')
+}
+module.exports.jupiter = function (application, req, res) {
+    res.render('jupiter')
+}
+module.exports.saturn = function (application, req, res) {
+    res.render('saturn')
+}
+module.exports.uranus = function (application, req, res) {
+    res.render('uranus')
+}
+module.exports.nepturne = function (application, req, res) {
+    res.render('neptune')
+}
