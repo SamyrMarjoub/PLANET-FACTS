3886ab84fa1b7b2ebe76022af87cc9b49ce25714
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-05-28 19:56:02 -0300

    Add files via upload

---
+++ app/public/js/script.js
@@ -0,0 +1,191 @@
+const Planet = document.querySelector('#js_img')
+const geology = document.querySelector('#geology')
+const text = document.querySelector('#js_text')
+const menu_img = document.querySelector('#menu-img')
+let width = window.innerWidth
+let cont = 1
+$(document).ready(function () {
+    Planet.style.transform = 'scale(1.5)'
+
+    setTimeout(function () {
+        Planet.style.transform = 'scale(1)'
+    }, 200)
+})
+
+$('#internal').click(function () {
+    Planet.setAttribute('src', '/images/planet-earth-internal.svg')
+    geology.style.display = 'none'
+    text.innerHTML = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle."
+    Planet.style.transform = 'scale(0.9)'
+    $('#internal').css('background-color', '#6d2ed5')
+    $('#overview').css('background-color', 'transparent')
+    $('#surface').css('background-color', 'transparent')
+    setTimeout(function () {
+        Planet.style.transform = 'scale(1)'
+    }, 200)
+
+})
+$('#overview').click(function () {
+    Planet.setAttribute('src', '/images/planet-earth.svg')
+    geology.style.display = 'none'
+    text.innerHTML = "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive."
+    Planet.style.transform = 'scale(0.9)'
+    $('#overview').css('background-color', '#6d2ed5')
+    $('#internal').css('background-color', 'transparent')
+    $('#surface').css('background-color', 'transparent')
+    setTimeout(function () {
+        Planet.style.transform = 'scale(1)'
+    }, 200)
+
+})
+$('#surface').click(function () {
+    geology.style.display = 'block'
+    Planet.setAttribute('src', '/images/planet-earth.svg')
+    text.innerHTML = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
+    $('#surface').css('background-color', '#6d2ed5')
+    $('#internal').css('background-color', 'transparent')
+    $('#overview').css('background-color', 'transparent')
+})
+$('#btn-m-f').click(function () {
+    $('.btn-mobile-1').css('border-bottom', '2px solid #6d2ed5')
+    $('.btn-mobile-2').css('border-bottom', 'none')
+    $('.btn-mobile-3').css('border-bottom', 'none')
+    Planet.setAttribute('src', '/images/planet-earth.svg')
+    geology.style.display = 'none'
+    text.innerHTML = "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive."
+    Planet.style.transform = 'scale(0.9)'
+    setTimeout(function () {
+        Planet.style.transform = 'scale(1)'
+    }, 200)
+})
+$('#btn-m-s').click(function () {
+    $('.btn-mobile-2').css('border-bottom', '2px solid #6d2ed5')
+    $('.btn-mobile-1').css('border-bottom', 'none')
+    $('.btn-mobile-3').css('border-bottom', 'none')
+    Planet.setAttribute('src', '/images/planet-earth-internal.svg')
+    geology.style.display = 'none'
+    text.innerHTML = "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle."
+    Planet.style.transform = 'scale(0.9)'
+    setTimeout(function () {
+        Planet.style.transform = 'scale(1)'
+    }, 200)
+})
+$('#btn-m-l').click(function () {
+    $('.btn-mobile-3').css('border-bottom', '2px solid #6d2ed5')
+    $('.btn-mobile-2').css('border-bottom', 'none')
+    $('.btn-mobile-1').css('border-bottom', 'none')
+    geology.style.display = 'block'
+    Planet.setAttribute('src', '/images/planet-earth.svg')
+    text.innerHTML = "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
+    $('#surface').css('background-color', '#6d2ed5')
+
+})
+$('.menu-mobile').click(function () {
+    ++cont
+    menu_img.setAttribute('src', '/images/icon-close-menu.svg')
+    if (cont % 2 == 0) {
+        if (width <= 320) {
+            $('body').css('overflow', 'visible')
+            $('html').css('overflow', 'visible')
+        }else{
+            $('body').css('overflow', 'hidden')
+            $('html').css('overflow', 'hidden')
+        }
+        $('.menu-mobile-div').show('fast')
+    } else {
+        $('.menu-mobile-div').hide('fast')
+        menu_img.setAttribute('src', '/images/icon-menu.svg')
+        $('body').css('overflow', 'visible')
+        $('html').css('overflow', 'visible')
+    }
+})
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("revolution-time");
+animateValue(obj, 0, 365, 1000);
+
+function animateValue2(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj2 = document.getElementById("rotation-time");
+animateValue2(obj2, 5, 1, 1000);
+
+function animateValue3(obj, start, end, duration) {
+    let startTimestamp = null;
+    let string = ''
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        string = Math.floor(progress * (end - start) + start);
+
+        obj.innerHTML = string
+
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj3 = document.getElementById("radius-1");
+animateValue3(obj3, 0, 6, 1000);
+
+function animateValue3_5(obj, start, end, duration) {
+    let startTimestamp = null;
+    let string = ''
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        string = Math.floor(progress * (end - start) + start);
+        obj.innerHTML = string
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj3_5 = document.querySelector(".radius-2");
+animateValue3_5(obj3_5, 0, 371, 1000);
+
+function animateValue4(obj, start, end, duration) {
+    let startTimestamp = null;
+    let string = ''
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        string = Math.floor(progress * (end - start) + start);
+
+        obj.innerHTML = string
+
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj4 = document.getElementById("temp");
+animateValue4(obj4, 0, 16, 1000);
