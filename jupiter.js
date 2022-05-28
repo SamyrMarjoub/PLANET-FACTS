74f9c62e7932792ed6f1b9059b439bbfa1483625
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-05-28 20:40:46 -0300

    Update jupiter.js

--- jupiter.js
+++ jupiter.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 })
 
 $('#internal').click(function () {
-    Planet.setAttribute('src', '/images/planet-jupiter-internal.svg')
+    Planet.setAttribute('src', './images/planet-jupiter-internal.svg')
     geology.style.display = 'none'
     text.innerHTML = `When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter's formation, which would have disrupted an originally solid Jovian core.`
     Planet.style.transform = 'scale(0.9)'
@@ -26,7 +26,7 @@ $('#internal').click(function () {
 
 })
 $('#overview').click(function () {
-    Planet.setAttribute('src', '/images/planet-jupiter.svg')
+    Planet.setAttribute('src', './images/planet-jupiter.svg')
     geology.style.display = 'none'
     text.innerHTML = `Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.`
     Planet.style.transform = 'scale(0.9)'
@@ -40,7 +40,7 @@ $('#overview').click(function () {
 })
 $('#surface').click(function () {
     geology.style.display = 'block'
-    Planet.setAttribute('src', '/images/planet-jupiter.svg')
+    Planet.setAttribute('src', './images/planet-jupiter.svg')
     text.innerHTML = `The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665.`
     $('#surface').css('background-color', '#d83a34')
     $('#internal').css('background-color', 'transparent')
@@ -50,7 +50,7 @@ $('#btn-m-f').click(function () {
     $('.btn-mobile-1').css('border-bottom', '2px solid #d83a34')
     $('.btn-mobile-2').css('border-bottom', 'none')
     $('.btn-mobile-3').css('border-bottom', 'none')
-    Planet.setAttribute('src', '/images/planet-jupiter.svg')
+    Planet.setAttribute('src', './images/planet-jupiter.svg')
     geology.style.display = 'none'
     text.innerHTML = `Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.`
     Planet.style.transform = 'scale(0.9)'
@@ -62,7 +62,7 @@ $('#btn-m-s').click(function () {
     $('.btn-mobile-2').css('border-bottom', '2px solid #d83a34')
     $('.btn-mobile-1').css('border-bottom', 'none')
     $('.btn-mobile-3').css('border-bottom', 'none')
-    Planet.setAttribute('src', '/images/planet-jupiter-internal.svg')
+    Planet.setAttribute('src', './images/planet-jupiter-internal.svg')
     geology.style.display = 'none'
     text.innerHTML = `When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter's formation, which would have disrupted an originally solid Jovian core.`
     Planet.style.transform = 'scale(0.9)'
@@ -75,14 +75,14 @@ $('#btn-m-l').click(function () {
     $('.btn-mobile-2').css('border-bottom', 'none')
     $('.btn-mobile-1').css('border-bottom', 'none')
     geology.style.display = 'block'
-    Planet.setAttribute('src', '/images/planet-jupiter.svg')
+    Planet.setAttribute('src', './images/planet-jupiter.svg')
     text.innerHTML = `The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665. `
     $('#surface').css('background-color', '#d83a34')
 
 })
 $('.menu-mobile').click(function () {
     ++cont
-    menu_img.setAttribute('src', '/images/icon-close-menu.svg')
+    menu_img.setAttribute('src', './images/icon-close-menu.svg')
     if (cont % 2 == 0) {
         if (width <= 320) {
             $('body').css('overflow', 'visible')
@@ -94,7 +94,7 @@ $('.menu-mobile').click(function () {
         $('.menu-mobile-div').show('fast')
     } else {
         $('.menu-mobile-div').hide('fast')
-        menu_img.setAttribute('src', '/images/icon-menu.svg')
+        menu_img.setAttribute('src', './images/icon-menu.svg')
         $('body').css('overflow', 'visible')
         $('html').css('overflow', 'visible')
     }
