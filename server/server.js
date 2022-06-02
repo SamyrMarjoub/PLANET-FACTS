048694be2581862ffd9352c489d9de3af9c79fd6
Samyr Marjoub <96318513+SamyrMarjoub@users.noreply.github.com>
2022-06-01 23:30:30 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 const express = require('express');
-/* importar o módulo do consign */
 const consign = require('consign');
-/* importar o módulo do body-parser */
-
 const app = express();
-/* setar as variáveis 'view engine' e 'views' do express */
+
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
-/* configurar o middleware express.static */
 app.use(express.static('./app/public'));
-/* configurar o middleware body-parser */
-/* configurar o middleware express-session */
 
 consign()
 	.include('app/routes')
